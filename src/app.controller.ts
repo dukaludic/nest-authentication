59b30b271344582d98af520a682ac63dac540b63
dukaludic <dukaludic@gmail.com>
2022-01-25 18:33:59 +0100

    JWT Strategy done for one endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Get, Post, Req, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req): any {
-    return req.user;
+    return this.authService.login(req.user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('protected')
   getHello(@Request() req): string {
     return req.user;
